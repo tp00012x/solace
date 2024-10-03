69c91dbcf6cffc45395f7dea4400b0ffaee72710
Anthony Torres <anthony_torres01@yahoo.com>
2024-10-03 15:34:09 -0500

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import React from "react";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/public/favicon.ico" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
